32a676bfb1cac136887adc62a985239c8783f377
JordannM83 <11457@holbertonstudents.com>
2025-12-22 15:49:03 +0100

    feat: add social media logos and refactor styles for consistency and responsiveness

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,9 +9,11 @@ function Footer() {
         <p>&copy; {currentYear} JordannM83. Tous droits réservés.</p>
         <div className="social-links">
           <a href="https://github.com/JordannM83" target="_blank" rel="noopener noreferrer">
+            <img src="/images/github_logo.png" alt="GitHub" className="social-icon" />
             GitHub
           </a>
-          <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
+          <a href="https://www.linkedin.com/in/jordann-miso-64a252255/" target="_blank" rel="noopener noreferrer">
+            <img src="/images/linkedin_logo.png" alt="LinkedIn" className="social-icon" />
             LinkedIn
           </a>
         </div>
