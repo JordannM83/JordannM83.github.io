3df59bc1ae7351eb7d9324243a400dc13e83193a
JordannM83 <11457@holbertonstudents.com>
2026-03-05 09:33:41 +0100

    feat: add CopyWebpackPlugin for asset copying and update package dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/index.tsx',
@@ -35,6 +36,14 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: './index.html'
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: 'public',
+          to: '.'
+        }
+      ]
     })
   ],
   devServer: {
